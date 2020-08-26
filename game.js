b09feab953e35f5d9341e049d790405ffe94efbf
Eugene Gogolev <stavrwalker@gmail.com>
2020-08-27 00:47:40 +0300

    Update game.js

--- game.js
+++ game.js
@@ -16,20 +16,20 @@ function Ball(x, y, rad, color) {
         context.closePath();
     }
     this.updateSelf = function () {
-        if(this.center_x + this.dx > canvas.width - this.radius || this.center_x + this.dx < this.radius) {
+        if (this.center_x + this.dx > canvas.width - this.radius || this.center_x + this.dx < this.radius) {
             this.dx = -this.dx;
-            this.color=getRandomColor()
+            this.color = getRandomColor()
         }
-        if(this.center_y + this.dy > canvas.height - this.radius || this.center_y + this.dy < this.radius) {
+        if (this.center_y + this.dy > canvas.height - this.radius || this.center_y + this.dy < this.radius) {
             this.dy = -this.dy;
-            this.color=getRandomColor()
+            this.color = getRandomColor()
         }
         this.center_x += this.dx
         this.center_y += this.dy
     }
 }
 
-function  createRandomBall() {
+function createRandomBall() {
     let radius = 15 + Math.floor(Math.random() * Math.floor(10));
     let x = radius + Math.random() * (canvas.width - (2 * radius));
     let y = radius + Math.random() * (canvas.height - (2 * radius));
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
 }, false)
 
 document.addEventListener("keydown", (e) => {
-    switch(e.code) {
+    switch (e.code) {
         case "Space":
             createRandomBall()
             break;
@@ -58,18 +58,32 @@ document.addEventListener("keydown", (e) => {
 })
 
 function update() {
-    canvas = document.getElementById("gameField");
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientHeight;
-    context = canvas.getContext("2d");
     draw()
-    balls.forEach(ball => {ball.updateSelf()})
+    balls.forEach(ball => {
+        ball.updateSelf()
+    })
+    balls.forEach(ball1 => {
+        balls.forEach(ball2 => {
+            if (ball2 === ball1) {
+                continue
+            }
+            if (dist(ball1.center_x, ball1.center_y,
+                ball2.center_x, ball2.center_y) <= ball1.radius + ball2.radius) {
+                ball1.dx *= -1
+                ball2.dx *= -1
+                ball1.dy *= -1
+                ball2.dy *= -1
+            }
+        })
+    })
     requestAnimationFrame(update)
 }
 
 function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    balls.forEach(ball => {ball.draw()})
+    balls.forEach(ball => {
+        ball.draw()
+    })
 }
 
 function getRandomColor() {
@@ -79,4 +93,8 @@ function getRandomColor() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
+}
+
+function dist(x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
 }
